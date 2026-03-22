03f9831d811ae8359f677ffcdfe27c47db64119d
Natsu Kagami <nki@nkagami.me>
2026-03-22 14:25:30 +0100

    Add CI to update Scala version

--- .github/scripts/update-build-version.sh
+++ .github/scripts/update-build-version.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 
-set -xe
+set -e
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+BUILD_FILE="$SCRIPT_DIR/../../build.sbt"
+export GIT_CONFIG="$SCRIPT_DIR/.gitconfig"
 
 LATEST_NIGHTLY=$("$SCRIPT_DIR"/latest-nightly.sc)
 
-sed -Ei 's/val scala3Version = ".+-NIGHTLY"/val scala3Version = "'$LATEST_NIGHTLY'"/g' "$SCRIPT_DIR/../../build.sbt"
+sed -Ei 's/val scala3Version = ".+-NIGHTLY"/val scala3Version = "'$LATEST_NIGHTLY'"/g' "$BUILD_FILE"
+
+if git diff --quiet "$BUILD_FILE"; then
+  echo "No changes."
+  exit 0
+fi
 
 echo "Latest nightly updated to $LATEST_NIGHTLY"
+
+git add "$BUILD_FILE"
+git commit -m "Update Scala version to $LATEST_NIGHTLY"
+git push
